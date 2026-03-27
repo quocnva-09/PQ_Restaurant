97b0028e3553122e68f03f7f72b7f8edd57d7e3d
quocnva <quocnva@runsystem.net>
2026-03-27 10:59:24 +0700

    add hello function index.js

--- src/index.js
+++ src/index.js
@@ -22,3 +22,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+hello();
+function hello(){
+  alert("Hello");
+}
